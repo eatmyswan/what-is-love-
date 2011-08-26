ad437e76fde30f24e92b3bfa9f45c58d9ddef055
Steve Fantastic <hello@stevefantastic.me>
2011-08-26 14:00:34 -0500

    more styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  
+  def controller?(*controller)
+    controller.include?(params[:controller])
+  end
+
+  def action?(*action)
+    action.include?(params[:action])
+  end
+  
+  def id?(*id)
+    id.include?(params[:id])
+  end
+
 end
