b8d4000c62a55e76bb0e2207651f6db091f342af
Steve Fantastic <hello@stevefantastic.me>
2011-08-26 10:35:28 -0500

    default.png;

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -27,7 +27,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
-     "/images/default.jpg"
+     "/images/default.png"
   end
 
   # Process files as they are uploaded:
