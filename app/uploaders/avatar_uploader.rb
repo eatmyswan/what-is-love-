a5ab62cab458ba13184949e531c804fd53722ad1
Steve Fantastic <hello@stevefantastic.me>
2011-08-26 10:57:41 -0500

    carrierwave processing errors turned on

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,11 +1,11 @@
 # encoding: utf-8
-
+require 'carrierwave/processing/mini_magick'
   
 class AvatarUploader < CarrierWave::Uploader::Base
-  
+  include CarrierWave::MiniMagick
 
   # Include RMagick or ImageScience support:
-  include CarrierWave::RMagick
+  # include CarrierWave::RMagick
   # include CarrierWave::ImageScience
 
   # Choose what kind of storage to use for this uploader:
