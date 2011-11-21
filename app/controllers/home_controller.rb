ef7d4516d7dd2179c087688c570a5aa727076b43
Steve Fantastic <steven.doyle@me.com>
2011-11-21 16:40:01 -0600

    new omniauth

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,13 @@
 class HomeController < ApplicationController
 
   def index
-
-      @inbox = Group.all
-      @personal = Group.all
-      @professional = Group.all
-
+    if !session[:user_id]
+      render 'login'
+    else
+      @inbox = Group.where(user_id: current_user.id).and(master_title: nil).first
+      @personal = Group.where(user_id: current_user.id).and(master_title: 'Personal')
+      @professional = Group.where(user_id: current_user.id).and(master_title: 'Professional')
+    end
   end
 
 end
