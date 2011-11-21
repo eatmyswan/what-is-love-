ef7b587e0d6c9e139b2341561e7a1254a215072c
Steve Fantastic <steven.doyle@me.com>
2011-11-21 17:48:35 -0600

    omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.production?
-    provider :twitter, 'AavCBytb8S9ndh2Ig3pw', 'pnjOvEkVQkQmEwq6oBCiXGpu4nJv3hxDtb7A91EV4'
+    provider :twitter, ENV['AavCBytb8S9ndh2Ig3pw'], ENV['pnjOvEkVQkQmEwq6oBCiXGpu4nJv3hxDtb7A91EV4']
   end
   if Rails.env.development?
-    provider :twitter, 'AavCBytb8S9ndh2Ig3pw', 'pnjOvEkVQkQmEwq6oBCiXGpu4nJv3hxDtb7A91EV4'
+    provider :twitter, ENV['AavCBytb8S9ndh2Ig3pw'], ENV['pnjOvEkVQkQmEwq6oBCiXGpu4nJv3hxDtb7A91EV4']
   end
 end
