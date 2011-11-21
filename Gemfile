51b10bfdd927a9295315e8f566ae230799972893
Steve Fantastic <steven.doyle@me.com>
2011-11-21 12:17:11 -0600

    prepping for production

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.1.0'
+gem 'rails', '~> 3.1.0'
 gem 'rake'
 gem 'bson_ext'
 gem 'mongoid'
--- config/application.rb
+++ config/application.rb
@@ -48,7 +48,6 @@ module Rpm
     
     # Enable the asset pipeline
     config.assets.enabled = true
-   
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
