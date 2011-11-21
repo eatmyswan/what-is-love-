5389b44978b58e67e7563406f66364275b075655
Steve Fantastic <steven.doyle@me.com>
2011-11-21 13:09:46 -0600

    getting it live

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,5 +11,72 @@ module ApplicationHelper
   def id?(*id)
     id.include?(params[:id])
   end
+  
+  def uploadify_user
+   session_key_name = Rails.application.config.session_options[:key]
+   %Q{
+      <script type='text/javascript'>
+        $(document).ready(function() {
+          $('#upload').uploadify({
+            script          : '#{ user_images_path(current_user) }',
+            fileDataName    : 'image[image]',
+            uploader        : '#{ asset_path('uploadify.swf') }',
+            cancelImg       : '',
+            fileDesc        : 'Images',
+            fileExt         : '*.png;*.jpg;*.gif',
+            sizeLimit       : #{10.megabytes},
+            queueSizeLimit  : 1,
+            multi           : true,
+            auto            : true,
+            wmode           : 'transparent',
+            hideButton      : true,
+            height          : 150,
+            width           : 159,
+            queueID       : 'queue',
+            scriptData      : {
+              '_http_accept': 'application/javascript',
+              '#{session_key_name}' : encodeURIComponent('#{u(cookies[session_key_name])}'),
+              'authenticity_token'  : encodeURIComponent('#{u(form_authenticity_token)}')
+            },
+            onComplete      : function(a, b, c, response){ eval(response) }
+          });
+        });
+      </script>
+    }.gsub(/[\n ]+/, ' ').strip.html_safe
+  end
+  
+  def uploadify_group
+    session_key_name = Rails.application.config.session_options[:key]
+    %Q{
+      <script type='text/javascript'>
+        $(document).ready(function() {
+          $('#upload').uploadify({
+            script          : '#{ group_path(@group) }',
+            fileDataName    : 'image[image]',
+            uploader        : '#{ asset_path('uploadify.swf') }',
+            cancelImg       : '',
+            fileDesc        : 'Images',
+            fileExt         : '*.png;*.jpg;*.gif',
+            sizeLimit       : #{10.megabytes},
+            queueSizeLimit  : 1,
+            multi           : true,
+            auto            : true,
+            wmode           : 'transparent',
+            hideButton      : true,
+            height          : 150,
+            width           : 159,
+            queueID       : 'queue',
+            scriptData      : {
+              '_http_accept': 'application/javascript',
+              '#{session_key_name}' : encodeURIComponent('#{u(cookies[session_key_name])}'),
+              'authenticity_token'  : encodeURIComponent('#{u(form_authenticity_token)}'),
+              '_method' : 'PUT'
+            },
+            onComplete      : function(a, b, c, response){ eval(response) }
+          });
+        });
+      </script>
+    }.gsub(/[\n ]+/, ' ').strip.html_safe
+  end
 
 end
