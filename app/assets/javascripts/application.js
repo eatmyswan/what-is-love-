e74045cd1ea6b49432022ce4cf904073ec998426
Steve Fantastic <steven.doyle@me.com>
2011-11-21 12:23:22 -0600

    prepping for production

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,13 +2,6 @@
 //= require jquery-ui
 //= require jquery_ujs
 //= require_self
+//= require_tree .
 //= require jquery.remotipart
 //= require swfobject
-//= require uploadify
-//= require fancybox
-//= require cluetip
-//= require group
-//= require group_task_updates
-//= require group_vision_updates
-//= require categories
-//= require calendar
