2dc9c5f6d3fcf55b59d5a4612fa30873aa836ea4
Steve Fantastic <steven.doyle@me.com>
2011-11-25 13:49:55 -0600

    adding fixes

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -42,51 +42,56 @@ $('ul.sortable').live("mouseover", function() {
 				checkCount();
 			}
 		});
+		
+		$('.category').droppable({
+			accept: ".task_wrap",
+			hoverClass: "drop_hover",
+			tolerance: "pointer",
+			over: function (e,ui){
+				console.log('over');
+			},
+			drop:function(event,ui){
+				console.log('drop');
+				ui.draggable.remove();
+				var taskId = $(ui.draggable).attr('id');
+				var catId = this.id;
+				$.ajax({
+					url: "/tasks/" +  taskId,
+					type: 'PUT',
+					data: $.param({task : { group_id: catId, parent_id: '' }}),
+					success: function() { 
+							var count = parseInt( $('#'+catId).find('.count_text').text() ) + 1;
+							$('#'+catId).find('.count_text').text(count);
+							checkCount();
+					}
+				});
+				//update group_id for all li's inside
+				var subTask = $(ui.draggable).find('li');
+				$(subTask).each(function(){
+					taskId = $(this).attr('id');
+					$.ajax({
+						url: "/tasks/" +  taskId,
+						type: 'PUT',
+						data: $.param({task : { group_id: catId }})
+					});
+				});
+		    }
+		});
 	}
 });
 	
-$('.category').droppable({
-	accept: ".task_wrap",
-	hoverClass: "drop_hover",
-	tolerance: "pointer",
-	over: function (e,ui){
-		console.log('over');
-	},
-	drop:function(event,ui){
-		console.log('drop');
-		ui.draggable.remove();
-		var taskId = $(ui.draggable).attr('id');
-		var catId = this.id;
-		$.ajax({
-			url: "/tasks/" +  taskId,
-			type: 'PUT',
-			data: $.param({task : { group_id: catId, parent_id: '' }}),
-			success: function() { 
-					var count = parseInt( $('#'+catId).find('.count_text').text() ) + 1;
-					$('#'+catId).find('.count_text').text(count);
-					checkCount();
-			}
+
+$('#personal,#professional').live("mouseover", function() {
+	if (!$(this).data("init")) {
+		$(this).data("init", true);	
+		$(this).sortable({
+			connectWith: '.connectedSortable',
+			items: 'a',
+			axis: 'y',
+			forcePlaceholderSize: true,
+			tolerance: 'pointer'
 		});
-		//update group_id for all li's inside
-		var subTask = $(ui.draggable).find('li');
-		$(subTask).each(function(){
-			taskId = $(this).attr('id');
-			$.ajax({
-				url: "/tasks/" +  taskId,
-				type: 'PUT',
-				data: $.param({task : { group_id: catId }})
-			});
-		});
-    }
-});
-	
-	
-$('#personal,#professional').sortable({
-	connectWith: '.connectedSortable',
-	items: 'a',
-	axis: 'y',
-	forcePlaceholderSize: true,
-	tolerance: 'pointer'
+	}
 });
 
 });
