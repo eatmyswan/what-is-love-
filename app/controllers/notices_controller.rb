b27b9d32687b0d377f6856db5a4c7e8c46dab67e
Gilbert <gilbertbgarza@gmail.com>
2012-01-15 05:21:42 -0600

    Order notices by latest first.

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -1,11 +1,11 @@
 class NoticesController < ApplicationController
   def latest
-    @notices = current_user.notices
+    @notices = current_user.notices.desc :created_at
     render :layout => false
   end
 
   def latest_panel
-    @notices = current_user.notices
+    @notices = current_user.notices.desc :created_at
   end
 
 end
