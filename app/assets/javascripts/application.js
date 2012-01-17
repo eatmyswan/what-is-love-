2eadd7a2e3ff549d28f9a1213de069a3e9528a61
Gilbert <gilbertbgarza@gmail.com>
2012-01-17 13:42:04 -0600

    Prevent backspace browsing. Closes #5

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,3 +6,20 @@
 //= require_self
 //= require_tree .
 
+// Prevent the backspace key from navigating back.
+$(document).keydown(function (e) {
+  var doPrevent;
+
+  if (e.keyCode == 8) {
+    var d = e.srcElement || e.target
+      , tagName = d.tagName.toUpperCase()
+    ;
+    if (tagName == 'INPUT' || tagName == 'TEXTAREA') {
+      doPrevent = d.readOnly || d.disabled;
+    }
+    else doPrevent = true;
+  }
+  else doPrevent = false;
+
+  if (doPrevent) e.preventDefault();
+});
